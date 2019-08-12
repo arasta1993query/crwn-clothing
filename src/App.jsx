564a4e30f00f637361a807a9ccab4b33ef8633b7
arasta1993query <alias1371@gmail.com>
2019-08-13 02:23:04 +0430

    adding header component and links to home , shop and contact

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import { from } from 'rxjs';
 //import components
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
+ 
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -17,6 +18,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/4.4 crown.svg.svg';
+
+import './header.style.scss';
+import { from } from 'rxjs';
+
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className='options' >
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/contact'>
+                CONTACT
+            </Link>
+        </div>
+    </div>
+);
+
+export default Header;
